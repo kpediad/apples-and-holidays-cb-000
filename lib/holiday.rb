906e3db0d30d64cbce05573e96a6428158f47949
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-09-21 20:24:46 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,7 @@ def all_supplies_in_holidays(holiday_hash)
     value_hash.each do |holiday, supplies|
       print_str = "  "
       holiday.to_s.split("_").each {|word| print_str << word.capitalize + " "}
-      print_str.chomp!(" ") << ": "
+      print_str.chomp!(" ") + ": "
       supplies.each {|supply| print_str << supply + ", "}
       puts print_str.chomp!(" ,")
     end
